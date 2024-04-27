9166b2a55e46e676211976b14094374ac08d791d
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-27 15:25:18 +0300

    Fixing apps

--- src/components/layout/product/Applications.tsx
+++ src/components/layout/product/Applications.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const Applications = () => {
   return (
-    <div className="mt-[8.5rem] mx-auto px-5 text-center">
-      <h2 className="text-5xl font-bold tracking-[-0.67px] max-lg:text-[40px]">Over 300+ integrations</h2>
-      <p className="mt-5 text-lg tracking-tighter">
+    <div className="mt-[9rem] mb-14 mx-auto px-5 text-center">
+      <h2 className="text-5xl -mt-2 font-bold tracking-[-1px]">Over 300+ integrations</h2>
+      <p className="mt-5 text-lg">
         Expand the capabilities of <span className="font-black">Roooby</span> with hundreds of apps and integrations
       </p>
 
-      <div className="mt-12 flex flex-wrap justify-center gap-2">
+      <div className="flex flex-wrap justify-center gap-2 mt-16 ">
         {logoApps.map((logo) => (
           <Image
             key={logo.id}
@@ -18,12 +18,12 @@ const Applications = () => {
             alt="app logo"
             width={0}
             height={0}
-            className="max-w-none w-max px-[32px] py-[24px] bg-white"
+            className="max-w-none w-max px-[43.4px] py-[24px] bg-white"
           />
         ))}
       </div>
 
-      <Link href="/prices" className="mt-24 block text-2xl font-medium max-lg:text-[1.375rem]">
+      <Link href="/prices" className="mt-20 block text-2xl font-medium max-lg:text-[1.375rem]">
         See all apps and extensions &gt;
       </Link>
     </div>
