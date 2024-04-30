ab51f783503b68833a0b767cbfea1e6b8d0d39a0
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-04-30 22:10:09 +0300

    Start work on Pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,6 +1,14 @@
 import Pricing from "@/components/layout/pricing/Pricing";
+import Section from "@/components/ui/Section";
+import Main from "@/components/ui/Main";
 
 
 export default function PricingPage() {
-  return <Pricing />;
+  return (
+    <Main>
+      <Section>
+        <Pricing />
+      </Section>
+    </Main>
+  )
 }
