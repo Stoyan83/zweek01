3b8b90ada0e244fd6da8c8cd43e34de20d7cb6ed
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-05-02 19:20:04 +0300

    Start working on BLog section

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+
+import Main from "@/components/ui/Main";
+import BlogList from "@/components/layout/blog/BlogList";
+
+export default function BlogPage() {
+  return (
+    <Main>
+      <BlogList />
+    </Main>
+  );
+}
---
+++ src/components/layout/blog/BlogList.tsx
@@ -0,0 +1,11 @@
+import Section from "@/components/ui/Section";
+
+const BlogList = () => {
+  return (
+    <Section fullWidth>
+        <h1>Blog...</h1>
+    </Section>
+  );
+};
+
+export default BlogList;
