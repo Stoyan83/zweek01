c79244cfa46ad4e272d74e4dc758bbe41072f8be
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2024-05-04 15:55:02 +0300

    adding backgorund

--- src/components/layout/product/Service.tsx
+++ src/components/layout/product/Service.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/ui/Button";
 import Image from "next/image";
 
 interface Bullet {
@@ -6,33 +7,58 @@ interface Bullet {
 }
 
 const Service = ({ item }: { item: any }) => (
-  <div key={item.id} className={`flex justify-between items-center max-sm:py-5 lg:pt-[3.125rem] border-t-[0.0625rem] border-placeholder-grey max-lg:flex-col max-lg:gap-2.5rem ${item.id === 2 ? 'lg:flex-row-reverse' : ''}`}>
-    <div className={`max-w-[30.625rem] ${item.id === 3 ? 'lg:pt-[3.8rem] max-sm:py-5' : ''}`}>
+  <div
+    key={item.id}
+    className={`relative flex justify-between items-center max-sm:py-5 lg:pt-[3.125rem] border-t-[0.0625rem] border-placeholder-grey max-lg:flex-col max-lg:gap-2.5rem ${
+      item.id === 2 ? "lg:flex-row-reverse" : ""
+    }`}
+  >
+    <div
+      className={`max-w-[30.625rem] ${
+        item.id === 3 ? "lg:pt-[3.8rem] max-sm:py-5" : ""
+      }`}
+    >
       <div className="px-2 py-[0.375rem] inline-block text-sm font-bold bg-benefit_yellow tracking-[0.875px] uppercase rounded ">
         {item.tag}
       </div>
       <h3 className="mt-[0.62rem] text-5xl font-bold leading-[1.2] tracking-[-0.042rem] max-lg:text-[2.5rem]">
         {item.title}
       </h3>
-      <p className="mt-6 text-lg leading-[1.75rem]">
-        {item.text.paragraph}
-      </p>
+      <p className="mt-6 text-lg leading-[1.75rem]">{item.text.paragraph}</p>
       <ul className="mt-5 space-y-4">
         {item.text.bullets.map((bullet: Bullet) => (
           <li key={bullet.id} className="flex gap-5">
-            <Image src="/shape.svg" alt="bullet shape" width={18}  height={18} className="mb-4 w-auto h-auto" />
+            <div>
+              <Image
+                src="/shape.svg"
+                alt="bullet shape"
+                width={18}
+                height={18}
+                className="mb-4 w-auto h-auto"
+              />
+            </div>
             <p className="text-lg tracking-[0.2px]">{bullet.text}</p>
           </li>
         ))}
       </ul>
     </div>
-    <div className={`relative -mt-2 max-sm:mt-12`}>
-      <Image
-        src={item.image}
-        alt={item.tag}
-        width={555}
-        height={555}
-      />
+    <div className="relative">
+      <div className="absolute bottom-0 left-0 w-full">
+        {item.id === 2 && (
+          <div className="mx-auto mb-4 lg:mb-8 lg:w-[450px] lg:h-[80px] bg-primary rounded-xl flex flex-col lg:flex-row items-center">
+            <p className="text-lg ml-2 font-bold lg:ml-4 lg:mr-4 tracking-[-0.25px] text-white whitespace-nowrap mb-2 lg:mb-0">
+              Automate your sales & marketing
+            </p>
+            <Button
+              primary
+              className="px-4 py-[10px] lg:py-4 lg:ml-2 font-bold text-lg bg-news-green rounded-lg"
+            >
+              Start now
+            </Button>
+          </div>
+        )}
+      </div>
+      <Image src={item.image} alt={item.tag} width={555} height={555} />
     </div>
   </div>
 );
--- src/components/layout/product/Services.tsx
+++ src/components/layout/product/Services.tsx
@@ -2,6 +2,7 @@ import { services } from "@/api/data";
 import Section from "@/components/ui/Section";
 import Service from "./Service";
 
+
 const Services = () => {
   return (
     <>
@@ -9,7 +10,11 @@ const Services = () => {
         <Section
           key={index}
           fullWidth
-          className={`lg:pt-[8.1rem] max-sm:py-5 -mt-2 max-lg:py-[7.5rem] bg-white ${index === 2 ? 'pt-[8rem] pb-[7.35rem] max-sm:pb-2' : 'pb-[-1.5rem] max-sm:pb-2'}`}
+          className={`lg:pt-[8.1rem] max-sm:py-5 -mt-2 max-lg:py-[7.5rem] bg-white ${
+            index === 2
+              ? "pt-[8rem] pb-[7.35rem] max-sm:pb-2"
+              : "pb-[-1.5rem] max-sm:pb-2"
+          }`}
         >
           <div className="max-w-[73.75rem] mx-auto px-5 space-y-[7.5rem] max-lg:space-y-[3.75rem]">
             <Service item={item} />
